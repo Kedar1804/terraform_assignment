b33d511a9bb682b6b472359fd7f6aff81be5cd53
Kedar1804 <khippalge@gmail.com>
2026-05-24 09:03:05 +0530

    commited

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -98,6 +98,31 @@ resource "aws_cloudwatch_log_group" "ecs" {
   retention_in_days = 7
 }
 
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
 resource "aws_ecs_task_definition" "app" {
   family                   = "backend-task"
   network_mode             = "awsvpc"
@@ -106,7 +131,7 @@ resource "aws_ecs_task_definition" "app" {
   cpu    = "256"
   memory = "512"
 
-  execution_role_arn = "arn:aws:iam::886492071931:role/ecsTaskExecutionRole"
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([
     {
@@ -159,4 +184,8 @@ resource "aws_ecs_service" "app" {
     container_name   = "backend-app"
     container_port   = 80
   }
+
+  depends_on = [
+    aws_ecs_cluster_capacity_providers.main
+  ]
 }
