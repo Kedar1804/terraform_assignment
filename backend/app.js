ada065e56a0f7c8f37753f9e4eee5443f255ccee
Kedar1804 <khippalge@gmail.com>
2026-05-24 12:05:45 +0530

    Cd

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("CI/CD Working Successfully"); 
+  res.send("Hello from backend CI/CD Working Successfully"); 
 });
 
 app.listen(80, () => {
